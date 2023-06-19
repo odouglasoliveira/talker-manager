edee52908867bac2fa0851a4e493bbbd88394d90
Douglas Oliveira <doglasolivera13@gmail.com>
2023-06-19 18:11:27 -0300

    Remoção do crypto random string do package-lock.json

--- src/middlewares/verifyEmail.js
+++ src/middlewares/verifyEmail.js
@@ -2,14 +2,13 @@ async function verifyEmail(req, res, next) {
   const { email } = req.body;
   const HTTP_ERROR_STATUS = 400;
   const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-  const isValid = emailRegex.test(email);
+  const isInvalid = !emailRegex.test(email);
   if (!email) {
     return res
     .status(HTTP_ERROR_STATUS)
     .json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!isValid) {
-    console.log(isValid);
+  if (isInvalid) {
     return res
     .status(HTTP_ERROR_STATUS)
     .json({ message: 'O "email" deve ter o formato "email@email.com"' });
